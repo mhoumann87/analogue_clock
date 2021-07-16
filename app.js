9e557061ecd218e66325d2f9641b91afe36e46a1
Michael Houmann <mhoumann87@gmail.com>
2021-07-16 10:36:07 +0200

    Add hands to the clock :sparkles:

--- app.js
+++ app.js
@@ -38,20 +38,24 @@ const drawFace = () => {
   // We use the grad variable to add gradient for the circle around the
   // clock face
   let grad;
+
   // Drawing of the clock face
   ctx.beginPath();
   ctx.arc(0, 0, radius, 0, 2 * Math.PI);
   ctx.fillStyle = 'White';
   ctx.fill();
+
   // Create the outside circle and add the gradient color to it
   grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
   grad.addColorStop(0, '#333');
   grad.addColorStop(0.5, 'White');
   grad.addColorStop(1, '#333');
+
   // Add the gradient to the circle and draw it
   ctx.strokeStyle = grad;
   ctx.lineWidth = radius * 0.1;
   ctx.stroke();
+
   //add the inner black circle to the face
   ctx.beginPath();
   ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
@@ -68,6 +72,7 @@ const drawNumbers = () => {
   ctx.font = `${radius * 0.15}px arial`;
   ctx.textBaseline = 'middle';
   ctx.textAlign = 'center';
+
   // For loop to add the numbers to the clock
   for (number = 1; number < 13; number++) {
     angle = (number * Math.PI) / 6;
@@ -86,12 +91,42 @@ const drawTime = () => {
   let hours = now.getHours();
   let minutes = now.getMinutes();
   let seconds = now.getSeconds();
+
   // Add the time as a "digital" clock below the canvas
   showTime.innerHTML = `<p>${hours.toString()} ${addZero(minutes)} ${addZero(
     seconds
   )}</p>`;
-  // Draw the line showing th hours
+
+  // Get the positions for the hands
   hours = hours % 12;
+
+  hours =
+    hours * (Math.PI / 6) +
+  minutes * (Math.PI / (6 * 60)) +
+  seconds * (Math.PI / (360 * 60));
+  console.log(hours);
+
+  // Draw the hand
+  drawHands(hours, radius * 0.7, radius * 0.07);
+
+  // get the position for the  minute hand and draw it
+  minutes = minutes * (Math.PI / 30) + seconds * (Math.PI / (30 * 60));
+  drawHands(minutes, radius * 0.8, radius * 0.07);
+
+  // get the position for the second hand and draw it
+  seconds = seconds * (Math.PI / 30);
+  drawHands(seconds, radius * 0.9, radius * 0.02);
+};
+
+const drawHands = (position, length, width) => {
+  ctx.beginPath();
+  ctx.lineWidth = width;
+  ctx.lineCap = 'round';
+  ctx.moveTo(0, 0);
+  ctx.rotate(position);
+  ctx.lineTo(0, -length);
+  ctx.stroke();
+  ctx.rotate(-position);
 };
 
 drawClock();
